8d096fd03871c38f2f0016c38716a7222c652a7c
Yauheni Susha <ergersusha2@gmail.com>
2021-12-30 14:51:40 +0300

    Moved build to repo root

--- flow-typed/core.js
+++
@@ -1,5 +0,0 @@
-// Various hacks to compensate for outdated Flow core definitions
-
-declare class Symbol extends Symbol {
-  static asyncIterator: string; // polyfill '@@asyncIterator'
-}
--- src/__testUtils__/resolveOnNextTick.js
+++
@@ -1,3 +0,0 @@
-export default function resolveOnNextTick(): Promise<void> {
-  return Promise.resolve(undefined);
-}
